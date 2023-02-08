38bdcc977a2efc5357effbb2e0fb723364c6c3e0
Martin Bonnin <martin@mbonnin.net>
2023-02-08 11:00:34 +0100

    update to 8.1.0-alpha3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("jvm") version "1.7.10" apply false
     kotlin("android") version "1.7.10" apply false
-    id("com.android.application") version "7.4.1" apply false
+    id("com.android.application") version "8.1.0-alpha03" apply false
     id("com.android.library") version "7.4.1" apply false
 }
 
